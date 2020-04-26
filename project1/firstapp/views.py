d6d5d6c47177d23bdf4b7e2649424a9a97689d08
aliensmart <kaoua17@gmail.com>
2020-04-26 02:22:50 -0400

    index modified

--- project1/firstapp/views.py
+++ project1/firstapp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from firstapp.models import Topic, AccessRecord, Webpage
 # Create your views here.
 def index(request):
-    my_dict = {
-        'the_text': 'this is a text from view'
+    webpage_list = AccessRecord.objects.order_by('date')
+    date_dict = {
+        'access_records': webpage_list
     }
-    return render(request, 'firstapp/index.html', context=my_dict)
+    return render(request, 'firstapp/index.html', context=date_dict)
